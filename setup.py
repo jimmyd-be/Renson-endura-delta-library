4b322253f1c61aead03a28fe6c7deda6e3b1a582
jimmyd-be <dom.jimmy@gmail.com>
2022-09-25 08:41:08 +0200

    Made breeze available for manual level. Changed parsing to level type

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ README = (HERE / "README.md").read_text()
 setup(
     name='renson_endura_delta',
     packages=find_packages(include=['renson_endura_delta']),
-    version='1.3.3',
+    version='1.3.4',
     description='Unofficial Renson endura delta Python library',
     long_description=README,
     long_description_content_type="text/markdown",
