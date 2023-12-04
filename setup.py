123576163acec81b794ce750f0b9977b6537abec
jimmyd-be <34766203+jimmyd-be@users.noreply.github.com>
2023-12-04 21:05:44 +0100

    Develop (#28)
    * Add missing import
    
    * Fixed bug for syncing time
    
    * Fixed bug for syncing time (#25)
    
    * Add restart device
    
    * Update version number
    
    * Split set_time to set_day_time and set_night_time
    
    * Fixed typo in enum

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ README = (HERE / "README.md").read_text()
 setup(
     name='renson_endura_delta',
     packages=find_packages(include=['renson_endura_delta']),
-    version='1.7.0',
+    version='1.7.1',
     description='Unofficial Renson endura delta Python library',
     long_description=README,
     long_description_content_type="text/markdown",
